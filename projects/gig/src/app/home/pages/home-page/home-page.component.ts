8821ea34660f8661a2d1eb7f13135d47be3e2f5f
Richard <rcarrion3406@gmail.com>
2022-06-03 11:19:40 -0500

    Login redirect

--- projects/gig/src/app/home/pages/home-page/home-page.component.ts
+++ projects/gig/src/app/home/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'gig-home-page',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+      if(sessionStorage.getItem("userid") === null){ 
+          this.router.navigate(['/login']);  
+      }
   }
 
 }
